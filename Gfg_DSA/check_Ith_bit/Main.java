e7acd59415968ef4b3432e9c8c23634cbefff9de
Sidharth Jain <imsidharthj@gmail.com>
2024-10-20 20:40:59 +0530

    bit oprations

--- Gfg_DSA/check_Ith_bit/Main.java
+++ Gfg_DSA/check_Ith_bit/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static boolean checkBitLeft(int n, int i) {
         return ((n & (1 << i)) != 0);
@@ -8,9 +10,10 @@ public class Main {
     }
 
     public static boolean setBit(int n, int i){
-        if ((n | (1 << i)) != 0){
-            return true;
-        }
+        // if ((n | (1 << i)) != 0){
+        //     return true;
+        // }
+        return (n | (1 << i)) != 0;
     }
 
     public static int clearBit(int n, int i){
@@ -26,26 +29,29 @@ public class Main {
     }
 
     public static boolean checkPowerOfTwo(int n){
-        if ((n & n - 1) == 0){
-            return true;
-        } else {
-            return false;
-        }
+        // if ((n & n - 1) == 0){
+        //     return true;
+        // } else {
+        //     return false;
+        // }
+        return n > 0 && (n & (n - 1)) == 0;
     }
 
     public static int countBits(int n){
         int count = 0;
-        while(n > 1){
-            if (n % 2 == 0){
-                // count++;
-                // n /= 2;
-                count += n & 1;
-                n = n >> 1;
-            }
+        while(n > 0){
+            // if (n % 2 == 0){
+            //     // count++;
+            //     // n /= 2;
+            //     count += n & 1;
+            //     n = n >> 1;
+            // }
+            count += (n & 1);
+            n = n >> 1;
         }
-        if (n == 1){
-            count += 1;
-        }
+        // if (n == 1){
+        //     count += 1;
+        // }
         return count;
     }
 
@@ -69,9 +75,38 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        int n = 13; // Binary: 1010
-        int i = 2;
-        System.out.println(checkBitLeft(n, i));  // True
-        System.out.println(checkBitRight(n, i)); // True
+        Scanner scanner = new Scanner(System.in);
+        
+        // Test checkBitLeft and checkBitRight
+        System.out.println("Enter a number n and bit position i for checking bits:");
+        int n = scanner.nextInt();
+        int i = scanner.nextInt();
+        System.out.println("Check bit using left shift (bit " + i + "): " + checkBitLeft(n, i));
+        System.out.println("Check bit using right shift (bit " + i + "): " + checkBitRight(n, i));
+
+        // Test setBit
+        System.out.println("Set bit at position " + i + ": " + setBit(n, i));
+
+        // Test clearBit
+        System.out.println("Clear bit at position " + i + ": " + clearBit(n, i));
+
+        // Test checkPowerOfTwo
+        System.out.println("Enter a number to check if it's a power of 2:");
+        int powerOfTwoCheck = scanner.nextInt();
+        System.out.println("Is " + powerOfTwoCheck + " a power of 2? " + checkPowerOfTwo(powerOfTwoCheck));
+
+        // Test countBits and countBits_
+        System.out.println("Enter a number to count set bits:");
+        int bitCountNumber = scanner.nextInt();
+        System.out.println("Number of set bits (using shift): " + countBits(bitCountNumber));
+        System.out.println("Number of set bits (Brian Kernighan's algorithm): " + countBits_(bitCountNumber));
+
+        // Test countFlips
+        System.out.println("Enter two numbers to count bit flips:");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println("Number of bit flips to convert " + a + " to " + b + ": " + countFlips(a, b));
+
+        scanner.close();
     }
 }
