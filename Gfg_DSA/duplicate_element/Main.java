4472246b2294c0cccf443af4c6825715166cce93
Sidharth Jain <imsidharthj@gmail.com>
2024-10-20 20:43:37 +0530

    finding non duplicate element in array where other elements are repeting twice and thrice

--- Gfg_DSA/duplicate_element/Main.java
+++ Gfg_DSA/duplicate_element/Main.java
@@ -1,13 +1,13 @@
 import java.util.Arrays;
 class Main{
-    public static int duplicateElement(int[] arr){
+    public static int nonDuplicateElement(int[] arr){
         int result = 0;
         for (int i = 0; i < arr.length; i++){
             result ^= arr[i];
         }
         return result;
     }
-    public static int singleNumber(int[] nums){
+    public static int nonRepeatingElement(int[] nums){
         // int result = 0;
         // for (int i = 0; i < 32; i++){
         //     int count = 0;
@@ -30,12 +30,27 @@ class Main{
         }
         return nums[nums.length-1];
     }
+
+    public static int bucketsElement(int[] arr){
+        // int [] ones = new int[arr.length];
+        // int [] twos = new int[arr.length];
+        int ones = 0, twos = 0;
+        for (int i = 0; i < arr.length; i++){
+            ones = (ones ^ arr[i]) & ~ twos;
+            twos = (twos ^ arr[i]) & ~ ones;
+        }
+        return ones;
+    }
+
     public static void main(String[] args) {
         int[] arr1 = {1, 2, 4, 2, 1, 4, 3};
         int[] arr2 = {1, 4, 3, 2, 4, 1, 3, 4, 1, 3};
-        int uniqueElement = duplicateElement(arr1);
-        int duplicateElement = singleNumber(arr2);
+        int[] arr3 = {2, 2, 3, 2};
+        int uniqueElement = nonDuplicateElement(arr1);
+        int duplicateElement = nonRepeatingElement(arr2);
+        int bucketResult = bucketsElement(arr3);
         System.out.println("The non-duplicate element is: " + uniqueElement);
         System.out.println("The non-duplicate element is: " + duplicateElement);
+        System.out.println("The non-repeating element using bucket method is: " + bucketResult);
     }
 }
