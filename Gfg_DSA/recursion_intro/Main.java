75daeb77c4fe6d701e579aa3378718752ef73a82
Sidharth Jain <imsidharthj@gmail.com>
2024-10-26 23:28:16 +0530

    reverse number using recursion and fibonacci

--- Gfg_DSA/recursion_intro/Main.java
+++ Gfg_DSA/recursion_intro/Main.java
@@ -38,13 +38,42 @@ class Main{
         }
         return n + sumNnumbers(n - 1);
     }
+    public static int[] reverseArray(int[] arr, int i, int j){
+        // int[] arr2 = new int[arr.length];
+        // int i = 0; int j = arr.length - 1;
+        // for(; i < arr.length && j >= 0; i++, j--){
+        //     arr2[i] = arr[j];
+        // }
+        // return arr2;
+        if (i >= j){
+            return arr;
+        }
+        int temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+        return reverseArray(arr, i +1, j-1);
+    }
+    public static int fibonacci(int n){
+        if(n <= 1){
+            return n;
+        }
+        int last = fibonacci(n - 1);
+        int secondLast = fibonacci(n - 2);
+        return last + secondLast;
+    }
     public static void main(String [] args){
         int n = 3;
         // printNtimes(n, 1);
         // print1ToN(n, 1);
         // printNto1(n);
         // backtrack(1, n);
-        int sum = sumNnumbers(n);
-        System.out.println(sum);
+        // int sum = sumNnumbers(n);
+        // System.out.println(sum);
+        // int[] arr = {1, 2, 3, 4, 5};
+        // reverseArray(arr, 0, arr.length - 1);
+        // for (int num : arr) {
+        //     System.out.print(num + " ");
+        // }
+        System.out.println("Fibonacci of " + n + " is: " + fibonacci(n));
     }
 }
