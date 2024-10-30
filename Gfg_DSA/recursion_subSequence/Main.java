aa187afa916c9e17e2aab0c8f7c1ffbaa4e5ca50
Sidharth Jain <imsidharthj@gmail.com>
2024-10-30 21:37:14 +0530

    find all sunSequences of given sum in Array

--- Gfg_DSA/recursion_subSequence/Main.java
+++ Gfg_DSA/recursion_subSequence/Main.java
@@ -46,16 +46,50 @@ class Main{
         tempArrayList.remove(tempArrayList.size() - 1);
         util(arr, index + 1, tempArrayList, result);
     }
+    public static void sumSubSequence(int[] arr, int index, int target, int sumTillNow, ArrayList<Integer> tempArrayList, ArrayList<ArrayList<Integer>> result){
+        if(sumTillNow == target){
+            result.add(new ArrayList<>(tempArrayList));
+            return;
+        }
+        if(index >= arr.length){
+            return;
+        }
+        tempArrayList.add(arr[index]);
+        sumSubSequence(arr, index + 1, target, sumTillNow + arr[index], tempArrayList, result);
+        tempArrayList.remove(tempArrayList.size() - 1);
+        sumSubSequence(arr, index + 1, target, sumTillNow, tempArrayList, result);
+    }
+    public static boolean singleSumSubSequence(int[] arr, int index, int target, int sumTillNow, ArrayList<Integer> tempArrayList){
+        if (index == arr.length) {
+            if (sumTillNow == target) {
+                System.out.println(tempArrayList);
+                return true;
+            }
+            return false;
+        }
+        tempArrayList.add(arr[index]);
+        if (singleSumSubSequence(arr, index + 1, target, sumTillNow + arr[index], tempArrayList)) {
+            return true;
+        }
+        tempArrayList.remove(tempArrayList.size() - 1);
+        if (singleSumSubSequence(arr, index + 1, target, sumTillNow, tempArrayList)) {
+            return true;
+        }
+        return false;
+    }
     public static void main(String [] args){
-        int n = 3;
-        int[] arr = {1, 2, 3, 4, 5};
-        // subSequences(arr);
-        int[] arr2 = {3, 1, 2};
+        // int n = 3;
+        // int[] arr = {1, 2, 3, 4, 5};
+        // // subSequences(arr);
+        // int[] arr2 = {3, 1, 2};
+        int n = 2;
+        int[] arr = {1, 2, 1};
         ArrayList<ArrayList<Integer>> result = new ArrayList<>();
         ArrayList<Integer> tempArrayList = new ArrayList<>();
-        util(arr2, 0, tempArrayList, result);
+        // sumSubSequence(arr, 0, n, 0, tempArrayList, result);
         for (ArrayList<Integer> subSequence : result) {
             System.out.println(subSequence);
         }
+        singleSumSubSequence(arr, 0, n, 0, tempArrayList);
     }
 }
