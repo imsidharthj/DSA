4358c728f02e8ff5b32ab45acdefaa7be365cd2f
Sidharth Jain <imsidharthj@gmail.com>
2024-11-04 14:55:10 +0530

    finding Sequence of excatly four numbers whose sum equal to target using two pointers

--- Gfg_DSA/hashing_fourSum/Main.java
+++ Gfg_DSA/hashing_fourSum/Main.java
@@ -32,6 +32,40 @@ class Main{
                 tempArrayList.remove(tempArrayList.size() - 1);
             }
     }
+    public static ArrayList<ArrayList<Integer>> sumFour(int[] arr, int target){
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        Arrays.sort(arr);
+        if(arr == null || arr.length == 0){
+            return result;
+        }
+        for(int i = 0; i < arr.length; i++){
+            if (i > 0 && arr[i] == arr[i - 1]) continue;
+            for(int j = i + 1; j < arr.length; j++){
+                if (j > i + 1 && arr[j] == arr[j - 1]) continue;
+                int target2 = target - arr[j] - arr[i];
+                int left = j  + 1;
+                int right = arr.length - 1;
+                while (left < right) {
+                    int target2_sum = arr[left] + arr[right];
+                    if(target2_sum < target2) left++;
+                    else if(target2_sum > target2) right--;
+                    else if(target2_sum == target2){
+                        ArrayList<Integer> tempArrayList = new ArrayList<>();
+                        tempArrayList.add(arr[i]);
+                        tempArrayList.add(arr[j]);
+                        tempArrayList.add(arr[left]);
+                        tempArrayList.add(arr[right]);
+                        result.add(tempArrayList);
+                        while (left < right && arr[left] == arr[left + 1]) left++;
+                        while (left < right && arr[right] == arr[right - 1]) right--;
+                        left++;
+                        right--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
     public static void main(String[] args){
         int[] arr = {2,2,2,2,2};
         int target = 8;
@@ -39,7 +73,7 @@ class Main{
         // ArrayList<ArrayList<Integer>> result = new ArrayList<>();
         // sumIndexs4(arr, target, 0, 0, new ArrayList<>(), result, new HashSet<>());
         // System.out.println(result);
-        ArrayList<ArrayList<Integer>> result = fourSum(arr, target);
+        ArrayList<ArrayList<Integer>> result = sumFour(arr, target);
         System.out.println("Unique Quadruplets: " + result);
     }
 }
