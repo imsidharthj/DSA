dea42fd908fd5ded6acb5e6bb5a9a5b0f9d86754
Sidharth Jain <imsidharthj@gmail.com>
2024-11-07 22:07:59 +0530

    array manipulations functions for shifting and searching elements

--- Gfg_DSA/array_manipulation/Main.java
+++ Gfg_DSA/array_manipulation/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 class Main{
     public static int largestElement(int[] arr){
         int i = 0;
@@ -48,18 +51,65 @@ class Main{
         }
         return (sum2 - sum);
     }
-    // public static int subArray(int[] arr){
-    //     // array = {1, 2, 3, 4, 5, 6, 7, 8}
-    //     int n = arr.length;
-    //     for (int i = 0; i < n; i++){
-    //         for(int j = i; j < n; j++){
-    //             for(int k = i; k < j; k++){
-    //                 System.out.println(arr[k]);
-    //             }
-    //         }
-    //     }
-    // }
-
+    public static void removeDuplicates(int[] arr){
+        int n = arr.length;
+        // for (int i = 0; i < n; i++){
+        //     for(int j = i; j < n; j++){
+        //         for(int k = i; k < j; k++){
+        //             System.out.println(arr[k]);
+        //         }
+        //     }
+        // }
+        int i = 0; int j = i + 1;
+        while (i < n && j < n) {
+            if(arr[j] != arr[i]){
+                i++;
+                arr[i] = arr[j];
+            }
+            j++;
+        }
+        for(int k = i + 1; k < n; k++){
+            arr[k] = 0;
+        }
+    }
+    public static void leftRotateArrayOnePlace(int[] arr){
+        int n = arr.length; int temp = arr[0];
+        for (int i = 0; i < n - 1; i++){
+            arr[i] = arr[i + 1];
+        }
+        arr[n - 1] = temp;
+    }
+    public static void leftRotateArrayDplaces(int[] arr, int places){
+        int n = arr.length;
+        int[] tempArrayList = new int[places];
+        for(int i = 0; i < places; i++){
+            tempArrayList[i] = arr[i];
+        }
+        for(int i = places; i < n; i++){
+            arr[i - places] = arr[i];
+        }
+        int temp = 0;
+        for(int i = (n - places); i < n; i++){
+            arr[i] = tempArrayList[temp];
+            temp++;
+        }
+        // for (int i = 0; i < places; i++) {
+        //     arr[n - places + i] = tempArray[i];
+        // }
+    }
+    public static void siftZeors(int[] arr){
+        int n = arr.length;
+        int temp = 0;
+        for (int i = 0; i < n; i++){
+            if(arr[i] != 0){
+                arr[temp] = arr[i];
+                temp++;
+            }
+        }
+        for(int i = temp; i < n; i++){
+            arr[i] = 0;
+        }
+    }
     public static void printSubarrays(int[] arr) {
         int n = arr.length;
 
@@ -78,16 +128,26 @@ class Main{
     }
 
     public static void main(String[] args) {
-        int[] arr3 = {10, 20, 30, 40, 50};
-        int secondLargest = secondLargest(arr3);
-        System.out.println("Second Largest: " + secondLargest);
-        int[] arr = {1, 2, 3, 5};
-        int missing = missingElement(arr);
-        System.out.println("Missing Element: " + missing);
-        int[] arr2 = {1, 2, 3, 4};
-        printSubarrays(arr2);
-        int[] arr4 = {1, 2, 3, 4, 5};
-        boolean isSorted = checkSorted(arr4);
-        System.out.println("Is Sorted: " + isSorted);
+        // int[] arr3 = {10, 20, 30, 40, 50};
+        // int secondLargest = secondLargest(arr3);
+        // System.out.println("Second Largest: " + secondLargest);
+        // int[] arr = {1, 2, 3, 5};
+        // int missing = missingElement(arr);
+        // System.out.println("Missing Element: " + missing);
+        // int[] arr2 = {1, 2, 3, 4};
+        // printSubarrays(arr2);
+        // int[] arr4 = {1, 2, 3, 4, 5};
+        // boolean isSorted = checkSorted(arr4);
+        // System.out.println("Is Sorted: " + isSorted);
+        // int[] arr = {1, 1, 2, 2, 3, 4, 4};
+        // removeDuplicates(arr);
+        // System.out.println(Arrays.toString(arr));
+        // int[] arr = {1, 2, 3, 4, 5};
+        // int n = 2;
+        // leftRotateArrayDplaces(arr, n);
+        // System.out.println(Arrays.toString(arr));
+        int [] arr = {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
+        siftZeors(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
