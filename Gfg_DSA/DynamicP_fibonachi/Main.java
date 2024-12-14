b0e334f01c88aedd51e6f5b2ef3293936123eaf8
Sidharth Jain <imsidharthj@gmail.com>
2024-12-14 23:14:24 +0530

    finding fibonachi of number using iterative dynamic programming

--- Gfg_DSA/DynamicP_fibonachi/Main.java
+++ Gfg_DSA/DynamicP_fibonachi/Main.java
@@ -8,14 +8,24 @@ class Main{
         return dp[n] = fibonachi(n - 1, dp) + fibonachi(n - 2, dp);
     }
 
+    public static int fibonachiItrately(int n){
+        int dp[] = new int[n + 1];
+        dp[0] = 0; dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
     public static void main(String[]args){
         int n = 10;
         int[] dp = new int[n + 1];
-        for(int i = 0; i <= n; i++){
-            dp[i] = -1;
-        }
         // dp[0] = 0; dp[1] = 1;
-        int result = fibonachi(n, dp);
+        // for(int i = 2; i <= n; i++){
+        //     dp[i] = -1;
+        // }
+        // int result = fibonachi(n, dp);
+        int result = fibonachiItrately(n);
         System.out.println("Fibonacci of " + n + " is " + result);
     }
 }
